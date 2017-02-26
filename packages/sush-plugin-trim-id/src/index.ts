f4ee705a9ebe45f81c744bee39e5d812c0bc864c
3846masa <3846masahiro+git@gmail.com>
2017-02-26 18:25:22 +0900

    fix: Change default params for plugin
    affects: sush-plugin-trim-id

--- packages/sush-plugin-trim-id/src/index.ts
+++ packages/sush-plugin-trim-id/src/index.ts
@@ -9,7 +9,7 @@ export interface TrimIdParams {
  * `SUSHPluginTrimId` trims head or tail from ID.
  */
 function SUSHPluginTrimId (
-  { head, tail }: TrimIdParams = { head: 0, tail: 0 }
+  { head = 0, tail = 0 }: TrimIdParams = {}
 ) {
   return ({ id, stock }: SUSHInfo) => {
     const newId = (!tail) ? id.slice(head) : id.slice(head, -1 * tail);
