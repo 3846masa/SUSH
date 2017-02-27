ce0db91644dd74ee2cd2b09f11c06a7af418d05e
3846masa <3846masahiro+git@gmail.com>
2017-02-27 12:17:57 +0900

    fix: Fix redirect via JavaScript
    affects: sush-plugin-redirect
    
    When not redirect until 1 sec, plugin try to redirect via JavaScript Location.

--- packages/sush-plugin-redirect/src/index.ts
+++ packages/sush-plugin-redirect/src/index.ts
@@ -7,23 +7,26 @@ function SUSHPluginRedirect (
   { fallback }: { fallback?: string } = {}
 ) {
   return ({ id, stock }: SUSHInfo) => {
-    const redirectUrl = stock.has(id) ? stock.get(id) : fallback;
+    return new Promise((resolve, reject) => {
+      const redirectUrl = stock.has(id) ? stock.get(id) : fallback;
 
-    // Throw error if not match
-    if (!redirectUrl) {
-      throw new Error(`"${id}" is not found.`);
-    }
+      // Throw error if not match
+      if (!redirectUrl) {
+        return reject(new Error(`"${id}" is not found.`));
+      }
 
-    // Redirect via refresh meta tag
-    const meta = document.createElement('meta');
-    meta.setAttribute('http-equiv', 'refresh');
-    meta.setAttribute('content', `0;URL=${redirectUrl}`);
-    document.head.appendChild(meta);
+      // Redirect via refresh meta tag
+      const meta = document.createElement('meta');
+      meta.setAttribute('http-equiv', 'refresh');
+      meta.setAttribute('content', `0;URL=${redirectUrl}`);
+      document.head.appendChild(meta);
 
-    // Redirect via JavaScript Location
-    (SUSHPluginRedirect as any)._location.href = redirectUrl;
-
-    return { id, stock } as SUSHInfo;
+      // Redirect via JavaScript Location
+      setTimeout(() => {
+        (SUSHPluginRedirect as any)._location.href = redirectUrl;
+        resolve({ id, stock });
+      }, 1000);
+    }) as Promise<SUSHInfo>;
   };
 };
 
